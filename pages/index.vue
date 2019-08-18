910a350f598df133390f33db24517426f13dc68b
Ken Morton Jr <kmorton15@gmail.com>
2019-08-18 17:55:03 -0400

    button now points to this github repo

--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,7 @@
       </h2>
       <board></board>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">
+        <a href="https://github.com/kjmj/tic-tac-toe/" target="_blank" class="button--green">
           GitHub
         </a>
       </div>
